ec71f4a94952b7d2022e10cac43c6cd713ff3bef
KATC <323160698@comunidad.unam.mx>
2025-02-27 13:44:11 -0600

    Update GestionLugaresPension.java

--- src/com/estacionamiento/GestionLugaresPension.java
+++ src/com/estacionamiento/GestionLugaresPension.java
@@ -1,6 +1,9 @@
 package com.estacionamiento;
 
 import javax.swing.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GestionLugaresPension 
 {
@@ -38,6 +41,31 @@ public class GestionLugaresPension
         }
     }
 
+    
+    public class Pension 
+    {
+        int lugar;
+        String duracion;
+        LocalDateTime tiempoInicio;
+        LocalDateTime tiempoFin;
+        double costo;
+
+        // Constructor y métodos necesarios
+    }
+    
+    private Map<Integer, Pension> pensionesActivas = new HashMap<>();
+
+    public void registrarPension(int lugar, String duracionSeleccionada) {
+        LocalDateTime tiempoInicio = TiempoSimulado.ahora();
+        LocalDateTime tiempoFin = calcularTiempoFin(tiempoInicio, duracionSeleccionada);
+        double costo = calcularCostoPension(duracionSeleccionada);
+
+        Pension nuevaPension = new Pension(lugar, duracionSeleccionada, tiempoInicio, tiempoFin, costo);
+        pensionesActivas.put(lugar, nuevaPension);
+        revisionLugaresPension[lugar] = true;
+    }
+
+    
     public void comprobarDisponibilidadLugar(int lugar, JPanel pantallaAnterior) 
     {
         if (revisionLugaresPension[lugar]) 
@@ -49,14 +77,69 @@ public class GestionLugaresPension
             int respuesta = JOptionPane.showConfirmDialog(null, "Lugar libre. ¡Enhorabuena!", "¿Confirmas que deseas este lugar?", JOptionPane.YES_NO_OPTION);
             if (respuesta == JOptionPane.YES_OPTION) 
             {
-                System.out.println("Has seleccionado Sí.");
+            	JOptionPane.showMessageDialog(null, "Has seleccionado Sí.");
                 revisionLugaresPension[lugar] = true;
                 ventanasPension.mostrarConfirmacionReserva(pantallaAnterior, lugar);
             } 
             else 
             {
-                System.out.println("Has seleccionado No. Elige otro lugar disponible.");
+            	JOptionPane.showMessageDialog(null, "Has seleccionado No. Elige otro lugar disponible.");
             }
         }
     }
+
+    private LocalDateTime calcularTiempoFin(LocalDateTime inicio, String duracion) {
+        switch (duracion) {
+            case "1 día":
+                return inicio.plusDays(1);
+            case "1 semana":
+                return inicio.plusWeeks(1);
+            case "1 mes":
+                return inicio.plusMonths(1);
+            default:
+                return inicio;
+        }
+    }
+
+    public double calcularCostoPension(String duracion) 
+    {
+        switch (duracion) 
+        {
+            case "1 día":
+                return 100.0;
+            case "1 semana":
+                return 600.0;
+            case "1 mes":
+                return 2000.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public boolean verificarBoleto(int numeroBoleto) 
+    {
+        if (pensionesActivas.containsKey(numeroBoleto)) 
+        {
+            Pension pension = pensionesActivas.get(numeroBoleto);
+            LocalDateTime tiempoActual = TiempoSimulado.ahora();
+            if (!tiempoActual.isBefore(pension.tiempoFin)) 
+            {
+                // Pensión finalizada, permitir salida
+                revisionLugaresPension[pension.lugar] = false;
+                pensionesActivas.remove(numeroBoleto);
+                return true;
+            } 
+            else 
+            {
+                // Pensión aún vigente
+                return false;
+            }
+        } 
+        else 
+        {
+            // Boleto no válido
+            return false;
+        }
+    }
+
 }

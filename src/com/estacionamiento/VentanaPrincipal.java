449b1a17d3c4f24b7131aa5a5c1b911af97fb6c1
“KevinAlanTC” <323160698@comunidad.unam.mx>
2025-02-27 23:15:20 -0600

    Actualización gráfica de VentanaPrincipal

--- src/com/estacionamiento/VentanaPrincipal.java
+++ src/com/estacionamiento/VentanaPrincipal.java
@@ -9,24 +9,26 @@ import java.time.format.DateTimeFormatter; // Biblioteca para formatear la hora 
 
 public class VentanaPrincipal extends JFrame 
 {
-	private static JFrame ventana;
-	private static JPanel primeraPantalla, elegirServicio, elegirSalida;
-	private static JLabel horaHMS;
-	
-	private static GestionLugaresEstacionamiento claseGLE;
+    private static JFrame ventana;
+    private static JPanel primeraPantalla, elegirServicio, elegirSalida, elegirCreditos;
+    private static JLabel horaHMS;
+    
+    private static GestionLugaresEstacionamiento claseGLE;
     private static GestionLugaresPension claseGLP;
     private static VentanasEstacionamiento claseVE;
     private static VentanasPension claseVP;
-	
-	public static void main(String[] args)
-	{
-		TiempoSimulado.iniciarReloj();
-		
-		ventana = new JFrame();
-		ventana.setTitle("Estacionamiento");
+    
+    public static void main(String[] args)
+    {
+        // Inicia el reloj simulado
+        TiempoSimulado.iniciarReloj();
+        
+        ventana = new JFrame();
+        ventana.setTitle("Estacionamiento");
         ventana.setSize(900, 600);
+        ventana.setLocationRelativeTo(null);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
+        
         claseGLE = new GestionLugaresEstacionamiento();
         claseVE = new VentanasEstacionamiento(claseGLE);
         claseGLE.setVentanasEstacionamiento(claseVE);
@@ -37,130 +39,238 @@ public class VentanaPrincipal extends JFrame
         
         mostrarPrimeraPantalla(null);
         ventana.setVisible(true); // Se muestra la ventana
-	}
-	
-	/*
-	 * A cada función se le pasa el panel de la pantalla anterior que estaba en el frame Ventana (usualmente el panel principal 
-	 *  de donde el llamado a la otra función) para poder ocultarlo y que sea visible el siguiente Panel. Si no hay panel de 
-	 *  procediencia se envía null como argumento
-	 */
-	
-	//Las líneas de dentro de esta función deberían ir al inicio de cada función, pero al ser varias funciones es mejor usarlas así
-	public static void ocultarPantallaAnterior(JPanel pantallaAnterior)
-	{
-		if (pantallaAnterior != null) 
+    }
+    
+    /*
+     * Cada función recibe el panel anterior (si existe) para ocultarlo y mostrar el nuevo.
+     */
+    
+    public static void ocultarPantallaAnterior(JPanel pantallaAnterior)
+    {
+        if (pantallaAnterior != null)
             ventana.remove(pantallaAnterior);
-	}
-	
-	// Las líneas dentro de esta función deberían ir al final de cada función, pero al ser varias funciones es mejor usarlas así
-	public static void mostrarPantallaActual(JPanel pantallaActual)
-	{
-		ventana.add(pantallaActual); // Se añade el panel principal de la función ya con todos los elementos a la ventana
-        ventana.revalidate(); // Actualiza el contenedor de la ventana
-        ventana.repaint(); // Redibuja la ventana
-	}
-	
-	public static void mostrarPrimeraPantalla(JPanel pantallaAnterior)
-	{
-		ocultarPantallaAnterior(pantallaAnterior);
-		
-		// Se instancia el panel principal de esta función
-		primeraPantalla = new JPanel();
-		primeraPantalla.setLayout(null);
-		
-		/*/ Se obtiene la hora y se le da formato
-		LocalTime horaActual = LocalTime.now();
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
+    }
+    
+    public static void mostrarPantallaActual(JPanel pantallaActual)
+    {
+        ventana.add(pantallaActual);
+        ventana.revalidate();
+        ventana.repaint();
+    }
+    
+    public static void mostrarPrimeraPantalla(JPanel pantallaAnterior)
+    {
+        ocultarPantallaAnterior(pantallaAnterior);
+        
+        // Se instancia el panel principal de esta función
+        primeraPantalla = new JPanel();
+        primeraPantalla.setLayout(null);
+        
+        // Se obtiene la hora y se le da formato
+        LocalTime horaActual = LocalTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
         String horaFormateada = horaActual.format(formato);
+        Font fuenteArial = new Font("Arial", Font.PLAIN, 25);
+        // Se instancia una etiqueta para mostrar la hora
+        horaHMS = new JLabel("" + horaFormateada);
+        horaHMS.setBounds(775, -28, 100, 80);
+        horaHMS.setFont(fuenteArial);
+        primeraPantalla.add(horaHMS);
         
-        // Se instancia una etiqueta para mostrar dicha hora 
-        horaHMS = new JLabel(""+ horaFormateada);
-        horaHMS.setBounds(50, 50, 100, 50);
-        primeraPantalla.add(horaHMS);*/
-		
-		// Instanciar el JLabel para el reloj simulado
-	    JLabel relojSimuladoLabel = new JLabel();
-	    relojSimuladoLabel.setBounds(700, 10, 200, 50); // Ajusta la posición y tamaño según sea necesario
-	    primeraPantalla.add(relojSimuladoLabel);
-
-	    // Timer para actualizar el reloj simulado cada segundo
-	    Timer actualizarReloj = new Timer(1000, e -> 
-	    {
-	        DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm");
-	        relojSimuladoLabel.setText("Tiempo Simulado: " + TiempoSimulado.ahora().format(formatoHora));
-	    });
-	    actualizarReloj.start();
-        
-        // Se intancia un botón para que el usuario registre su entrada
+        // Botón para registrar entrada
         JButton botonEntrada = new JButton("Entrada");
         botonEntrada.addActionListener(e -> mostrarPantallaEntrada(primeraPantalla));
-        botonEntrada.setBounds(50, 150, 200, 50);
+        botonEntrada.setBounds(145, 211, 210, 60);
+        botonEntrada.setBackground(Color.decode("#4C5C68"));
+        botonEntrada.setForeground(Color.decode("#FFFFFF"));
         primeraPantalla.add(botonEntrada);
         
-        // Se instancia un botón para que el usuario registre la salida
+        // Botón para registrar salida
         JButton botonSalida = new JButton("Salida");
         botonSalida.addActionListener(e -> mostrarPantallaSalida(primeraPantalla));
-        botonSalida.setBounds(251, 150, 200, 50);
+        botonSalida.setBounds(515, 211, 210, 60);
+        botonSalida.setBackground(Color.decode("#4C5C68"));
+        botonSalida.setForeground(Color.decode("#FFFFFF"));
         primeraPantalla.add(botonSalida);
         
+        // Botón para mostrar créditos
+        JButton botonCreditos = new JButton("Créditos");
+        botonCreditos.addActionListener(e -> mostrarPantallaCreditos(primeraPantalla));
+        botonCreditos.setBounds(45, 482, 150, 48);
+        botonCreditos.setBackground(Color.decode("#4C5C68"));
+        botonCreditos.setForeground(Color.decode("#FFFFFF"));
+        primeraPantalla.add(botonCreditos);
+        
+        // Botón para cerrar el programa
+        JButton cerrar = new JButton("Cerrar programa");
+        cerrar.addActionListener(e -> System.exit(0));
+        cerrar.setBounds(678, 507, 190, 44);
+        cerrar.setBackground(Color.decode("#4C5C68"));
+        cerrar.setForeground(Color.decode("#FFFFFF"));
+        primeraPantalla.add(cerrar);
+        
+        // Imagen de fondo
+        ImageIcon PrimeraPantallaImg = new ImageIcon(new ImageIcon("PrimeraPantalla.png").getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT));
+        JLabel etiPro1 = new JLabel();
+        etiPro1.setIcon(PrimeraPantallaImg);
+        etiPro1.setBounds(-15, -30, 900, 600);
+        primeraPantalla.add(etiPro1);
+        
         mostrarPantallaActual(primeraPantalla);
-	}
-	
-	private static void mostrarPantallaEntrada(JPanel pantallaAnterior)
-	{
-		ocultarPantallaAnterior(pantallaAnterior);
-		
-		// Se instancia el panel principal de esta función
-		elegirServicio = new JPanel();
-		elegirServicio.setLayout(null);
-		
-		// Se intancia en una etiqueta la instrucción que debe seguir el usuario
-		JLabel instruccion = new JLabel("Seleciona la opción de tu preferencia:");
-		instruccion.setBounds(10, 10, 250, 50);
-		elegirServicio.add(instruccion);
-		
-		// Se instancia un botón para que el usuario elija un lugar en el estacionamiento
-		JButton lugarEstacionamiento = new JButton("Quisiera un lugar en el estacionamiento");
-		lugarEstacionamiento.addActionListener(e -> claseVE.mostrarLugaresEstacionamiento(elegirServicio));
-		lugarEstacionamiento.setBounds(50, 200, 400, 100);
-		elegirServicio.add(lugarEstacionamiento);
-		
-		// Se instancia un botón para que el usario elija un lugar de pensión
-		JButton lugarPension = new JButton("Quisera un lugar de pensión");
-		lugarPension.addActionListener(e -> claseVP.mostrarLugaresPension(elegirServicio));
-		lugarPension.setBounds(50, 400, 400, 100);
-		elegirServicio.add(lugarPension);
-		
-		mostrarPantallaActual(elegirServicio);
-	}
-	
-	
-	private static void mostrarPantallaSalida(JPanel pantallaAnterior)
-	{
-		ocultarPantallaAnterior(pantallaAnterior);
-		
-		// Se instancia el panel principal de esta función
-		elegirSalida = new JPanel();
-		elegirSalida.setLayout(null);
-		
-		// Se intancia en una etiqueta la instrucción que debe seguir el usuario
-		JLabel instruccion = new JLabel("Seleciona la opción que deseas:");
-		instruccion.setBounds(10, 10, 250, 50);
-		elegirSalida.add(instruccion);
-		
-		// Se instancia un botón para que el usuario elija salir del estacionamiento
-		JButton salidaEstacionamiento = new JButton("Voy a salir del estacionamiento");
-		salidaEstacionamiento.addActionListener(e -> claseVE.mostrarSalidaEstacionamiento(elegirSalida));
-		salidaEstacionamiento.setBounds(50, 200, 400, 100);
-		elegirSalida.add(salidaEstacionamiento);
-		
-		// Se instancia un botón para que el usario elija recoger su auto de pensión
-		JButton salidaPension = new JButton("Voy a recoger mi coche de pensión");
-		salidaPension.addActionListener(e -> claseVP.mostrarSalidaPension(elegirSalida));
-		salidaPension.setBounds(50, 400, 400, 100);
-		elegirSalida.add(salidaPension);
-		
-		mostrarPantallaActual(elegirSalida);
-	}
-	
+    }
+    
+    public static void mostrarPantallaEntrada(JPanel pantallaAnterior)
+    {
+        ocultarPantallaAnterior(pantallaAnterior);
+        
+        // Se instancia el panel principal para la entrada
+        elegirServicio = new JPanel();
+        elegirServicio.setLayout(null);
+        
+        // Botón para solicitar un lugar en el estacionamiento
+        JButton lugarEstacionamiento = new JButton("Quisiera un lugar en el estacionamiento");
+        lugarEstacionamiento.addActionListener(e -> claseVE.mostrarLugaresEstacionamiento(elegirServicio));
+        lugarEstacionamiento.setBounds(55, 232, 345, 76);
+        lugarEstacionamiento.setBackground(Color.decode("#4C5C68"));
+        lugarEstacionamiento.setForeground(Color.decode("#FFFFFF"));
+        elegirServicio.add(lugarEstacionamiento);
+        
+        // Botón para solicitar un lugar de pensión
+        JButton lugarPension = new JButton("Quisiera un lugar de pensión");
+        lugarPension.addActionListener(e -> claseVP.mostrarLugaresPension(elegirServicio));
+        lugarPension.setBounds(496, 232, 345, 76);
+        lugarPension.setBackground(Color.decode("#4C5C68"));
+        lugarPension.setForeground(Color.decode("#FFFFFF"));
+        elegirServicio.add(lugarPension);
+        
+        // Botón para regresar al menú principal
+        JButton regresar = new JButton("Regresar al Menú Principal");
+        regresar.addActionListener(e -> mostrarPrimeraPantalla(elegirServicio));
+        regresar.setBounds(677, 455, 190, 44);
+        regresar.setBackground(Color.decode("#4C5C68"));
+        regresar.setForeground(Color.decode("#FFFFFF"));
+        elegirServicio.add(regresar);
+        
+        // Botón para cerrar el programa
+        JButton cerrar = new JButton("Cerrar programa");
+        cerrar.addActionListener(e -> System.exit(0));
+        cerrar.setBounds(677, 507, 190, 44);
+        cerrar.setBackground(Color.decode("#4C5C68"));
+        cerrar.setForeground(Color.decode("#FFFFFF"));
+        elegirServicio.add(cerrar);
+        
+        // Se muestra la hora actual en la pantalla de entrada
+        LocalTime horaActual = LocalTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        String horaFormateada = horaActual.format(formato);
+        Font fuenteArial = new Font("Arial", Font.PLAIN, 25);
+        horaHMS = new JLabel("" + horaFormateada);
+        horaHMS.setBounds(730, -24, 100, 80);
+        horaHMS.setFont(fuenteArial);
+        elegirServicio.add(horaHMS);
+        
+        // Imagen de fondo para la pantalla de entrada
+        ImageIcon PantallaEntradaImg = new ImageIcon(new ImageIcon("PantallaEntrada.png").getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT));
+        JLabel etiPro1 = new JLabel();
+        etiPro1.setIcon(PantallaEntradaImg);
+        etiPro1.setBounds(-15, -30, 900, 600);
+        elegirServicio.add(etiPro1);
+        
+        mostrarPantallaActual(elegirServicio);
+    }
+    
+    public static void mostrarPantallaSalida(JPanel pantallaAnterior)
+    {
+        ocultarPantallaAnterior(pantallaAnterior);
+        
+        // Se instancia el panel principal para la salida
+        elegirSalida = new JPanel();
+        elegirSalida.setLayout(null);
+        
+        // Se muestra la hora actual en la pantalla de salida
+        LocalTime horaActual = LocalTime.now();
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        String horaFormateada = horaActual.format(formato);
+        Font fuenteArial = new Font("Arial", Font.PLAIN, 25);
+        horaHMS = new JLabel("" + horaFormateada);
+        horaHMS.setBounds(730, -24, 100, 80);
+        horaHMS.setFont(fuenteArial);
+        elegirSalida.add(horaHMS);
+        
+        // Botón para salir del estacionamiento
+        JButton salidaEstacionamiento = new JButton("Voy a salir del estacionamiento");
+        salidaEstacionamiento.addActionListener(e -> claseVE.mostrarSalidaEstacionamiento(elegirSalida));
+        salidaEstacionamiento.setBounds(55, 232, 345, 76);
+        salidaEstacionamiento.setBackground(Color.decode("#4C5C68"));
+        salidaEstacionamiento.setForeground(Color.decode("#FFFFFF"));
+        elegirSalida.add(salidaEstacionamiento);
+        
+        // Botón para recoger el coche de pensión
+        JButton salidaPension = new JButton("Voy a recoger mi coche de pensión");
+        salidaPension.addActionListener(e -> claseVP.mostrarSalidaPension(elegirSalida));
+        salidaPension.setBounds(496, 232, 345, 76);
+        salidaPension.setBackground(Color.decode("#4C5C68"));
+        salidaPension.setForeground(Color.decode("#FFFFFF"));
+        elegirSalida.add(salidaPension);
+        
+        // Botón para regresar al menú principal
+        JButton regresar = new JButton("Regresar al Menú Principal");
+        regresar.addActionListener(e -> mostrarPrimeraPantalla(elegirSalida));
+        regresar.setBounds(677, 455, 190, 44);
+        regresar.setBackground(Color.decode("#4C5C68"));
+        regresar.setForeground(Color.decode("#FFFFFF"));
+        elegirSalida.add(regresar);
+        
+        // Botón para cerrar el programa
+        JButton cerrar = new JButton("Cerrar programa");
+        cerrar.addActionListener(e -> System.exit(0));
+        cerrar.setBounds(677, 507, 190, 44);
+        cerrar.setBackground(Color.decode("#4C5C68"));
+        cerrar.setForeground(Color.decode("#FFFFFF"));
+        elegirSalida.add(cerrar);
+        
+        // Imagen de fondo para la pantalla de salida
+        ImageIcon PantallaSalidaImg = new ImageIcon(new ImageIcon("PantallaSalida.png").getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT));
+        JLabel etiPro1 = new JLabel();
+        etiPro1.setIcon(PantallaSalidaImg);
+        etiPro1.setBounds(-15, -30, 900, 600);
+        elegirSalida.add(etiPro1);
+        
+        mostrarPantallaActual(elegirSalida);
+    }
+    
+    private static void mostrarPantallaCreditos(JPanel pantallaAnterior)
+    {
+        ocultarPantallaAnterior(pantallaAnterior);
+        
+        // Se instancia el panel para los créditos
+        elegirCreditos = new JPanel();
+        elegirCreditos.setLayout(null);
+        
+        // Botón para regresar al menú principal
+        JButton regresar = new JButton("Regresar al Menú Principal");
+        regresar.addActionListener(e -> mostrarPrimeraPantalla(elegirCreditos));
+        regresar.setBounds(677, 455, 190, 44);
+        regresar.setBackground(Color.decode("#4C5C68"));
+        regresar.setForeground(Color.decode("#FFFFFF"));
+        elegirCreditos.add(regresar);
+        
+        // Botón para cerrar el programa
+        JButton cerrar = new JButton("Cerrar programa");
+        cerrar.addActionListener(e -> System.exit(0));
+        cerrar.setBounds(677, 507, 190, 44);
+        cerrar.setBackground(Color.decode("#4C5C68"));
+        cerrar.setForeground(Color.decode("#FFFFFF"));
+        elegirCreditos.add(cerrar);
+        
+        // Imagen de fondo para la pantalla de créditos
+        ImageIcon creditosImg = new ImageIcon(new ImageIcon("Creditos.png").getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT));
+        JLabel etiPro1 = new JLabel();
+        etiPro1.setIcon(creditosImg);
+        etiPro1.setBounds(-15, -30, 900, 600);
+        elegirCreditos.add(etiPro1);
+        
+        mostrarPantallaActual(elegirCreditos);
+    }
 }
+
